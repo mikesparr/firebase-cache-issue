4aec5ba5b28171c03bac390783fc3cee2d0ec517
Mike Sparr <msparr@pointsixsolutions.com>
2018-07-26 10:29:28 -0600

    Updated tests to include simulation and rules to include simulation index

--- __tests__/app-spec.js
+++ __tests__/app-spec.js
@@ -1,5 +1,7 @@
-var admin = require('firebase-admin');
-var serviceAccount = require('../service-key.json');
+const admin = require('firebase-admin');
+const moment = require('moment');
+const serviceAccount = require('../service-key.json');
+const uuid = require('uuid/v1');
 
 const fbAdmin = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -109,4 +111,180 @@ describe("firebase", () => {
                 done.fail(error);
             });
     });
+
+    describe("app simulation", () => {
+        let originalTimeout; // increase timeout for batch job test and replace
+        let randomDeleteKeys = [];
+        const startDate = `2017-01-01`;
+
+        beforeAll((done) => {
+            const TransTypes = {
+                INT_CHARGE: 'Interest Charge',
+                INT_CREDIT: 'Interest Credit',
+                DEPOSIT: 'Deposit',
+                ADVANCE: 'Advance'
+            };
+            const MAX_RECS = 150000;
+            const CHUNK_SIZE = 1000;
+            const SAMPLE_SIZE = 50; // randomly pick a key for deletion and query
+
+            // update timeout interval for test suite
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 480000;
+
+            const _handleChunks = async (chunks) => {
+                console.log(`Handling ${chunks.length} chunks ...`)
+                await Promise.all(chunks.map(async chunk => {
+                    await db.ref().update(chunk);
+                    console.log(`*********** Processed chunk *************`);
+                }))
+                console.log(`Finished chunks`);
+            }
+    
+            const newRec = (id, chunk = 0) => {
+                return {
+                    account_id : '1932-324sdf-324sdf-124124',
+                    account_interest_bal : 100.0,
+                    account_name : 'Test account',
+                    account_principal_bal : 1000.0,
+                    amount: 3.24,
+                    description : 'Daily interest computation',
+                    id : id,
+                    object : 'transaction',
+                    org_available_funds : 10000.0,
+                    org_id : 'org-1348293402-23xvasdf-werd',
+                    owner : 'System',
+                    status : 'approved',
+                    type : TransTypes.INT_CHARGE,
+                    rate: 0.0475,
+                    published: moment.utc(startDate).add(chunk, 'day').endOf('day').valueOf(),
+                    updated : moment.utc(startDate).add(chunk, 'day').endOf('day').valueOf(),
+                    user : "System"
+                }
+            }
+    
+            const getTestRecordChunks = () => {
+                const transChunks = [];
+                let currentChunk = {};
+                
+                for (let i = 0; i < MAX_RECS; i++) {
+                    const id = uuid();
+
+                    let chunkCount = 0;
+                    currentChunk[`transactions/${id}`] = newRec(id, chunkCount);
+
+                    // pick 'pseudorandom' keys for deletion and test later
+                    if (i % SAMPLE_SIZE == 0) {
+                        randomDeleteKeys.push(id);
+                    }
+    
+                    if (i % CHUNK_SIZE == 0 || i == MAX_RECS) {
+                        transChunks.push(currentChunk);
+                        chunkCount ++; // used for incrementing date to allow a range query of subset of dates
+                        currentChunk = {};
+                    }
+                }
+    
+                return transChunks;
+            }
+
+            Promise.all([ _handleChunks( getTestRecordChunks() ) ])
+                .then(values => {
+                    console.log(`Successfully updated database with chunks`);
+                    done();
+                })
+                .catch(error => {
+                    console.error(`Error updating database`, error);
+                    done.fail(error);
+                });
+        });
+
+        afterAll((done) => {
+            // restore timeout interval for test suite
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+
+            // empty random delete keys array
+            randomDeleteKeys = [];
+
+            db.ref(`transactions`).remove()
+                .then((_) => {
+                    console.log(`Removed test transactions`);
+                    done();
+                })
+                .catch((error) => {
+                    console.log(`Error removing test transactions`, error);
+                    done.fail(error);
+                });
+        });
+
+        it("queries index for subset of data", (done) => {
+            const start = moment.utc(startDate).valueOf();
+            const end = moment.utc(startDate).add(30, 'days').valueOf();
+            const limitQuery = db.ref('transactions').orderByChild("published").startAt(start).endAt(end);
+
+            limitQuery
+                .once('value')
+                .then((snapshot) => {
+                    const records = snapshot.val();
+                    const resultLength = Object.keys(records).length;
+    
+                    console.log(`Found ${resultLength} records from query!`);
+                    done();
+                })
+                .catch((error) => {
+                    done.fail(error);
+                });
+        });
+
+        it("removes sampled keys using bulk update feature", (done) => {
+            // build dictionary of keys to bulk delete [set to null]
+            const updateData = {};
+            randomDeleteKeys.map((key) => updateData[`transactions/${key}`] = null);
+
+            db.ref().update(updateData)
+                .then(() => {
+                    console.log(`Set ${randomDeleteKeys.length} keys to null`);
+                    done();
+                })
+                .catch((error) => {
+                    console.error(`Error bulk removing sampled records`, error);
+                    done.fail(error);
+                });
+        });
+
+        it("queries subset and sampled keys should NOT exist", (done) => {
+            const start = moment.utc(startDate).valueOf();
+            const end = moment.utc(startDate).add(30, 'days').valueOf();
+            const limitQuery = db.ref('transactions').orderByChild("published").startAt(start).endAt(end);
+
+            limitQuery
+                .once('value')
+                .then((snapshot) => {
+                    const records = snapshot.val();
+                    const resultLength = Object.keys(records).length;
+                    let deletedRecordFound = false;
+    
+                    console.log(`Found ${resultLength} records from query!`);
+                    
+                    // loop through deleted keys and make sure none exist in results
+                    randomDeleteKeys.map((key) => {
+                        const match = records[key];
+                        if (match && match.id == key) {
+                            console.log(`Record: ${key} found in results but should not exist`);
+                            deletedRecordFound = true;
+                        }
+                    });
+
+                    if (!deletedRecordFound) {
+                        done();
+                    } else {
+                        done.fail(new Error(`Deleted records appeared in search results`));
+                    }
+                })
+                .catch((error) => {
+                    done.fail(error);
+                });
+        });
+
+    }); // simulation
 });
